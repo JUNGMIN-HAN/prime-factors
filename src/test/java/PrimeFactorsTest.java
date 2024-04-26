c0cee77d30c1a2608f2ef7225bb8c9ef574381d7
jungmin <gmlsajfl93@gmail.com>
2024-04-26 17:12:20 +0900

    [refactoring] 33

--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -1,20 +1,26 @@
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PrimeFactorsTest {
+class PrimeFactorsTest {
+
+    PrimeFactor primeFactor;
+
+    @BeforeEach
+    public void setup() throws Exception{
+        primeFactor = new PrimeFactor();
+    }
+
     @Test
     void testPrimefactorof1() {
-        PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(Arrays.asList(),primeFactor.of(1));
     }
 
     @Test
     void testPrimefactorof2() {
-        PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(Arrays.asList(2),primeFactor.of(2));
     }
 }
