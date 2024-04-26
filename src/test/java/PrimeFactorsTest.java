c42294c36778c7f3d87909bd87301df5f4388ff2
jungmin <gmlsajfl93@gmail.com>
2024-04-26 16:54:36 +0900

    Initial

---
+++ src/test/java/PrimeFactorsTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PrimeFactorsTest {
+    @Test
+    void testPrimefactorof1() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(10,10);
+    }
+}
