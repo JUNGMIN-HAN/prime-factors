ae934399e43a29acf8a75ff0adf366db2578bb61
jungmin <gmlsajfl93@gmail.com>
2024-04-29 09:06:25 +0900

    [feature] 54

--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -43,4 +43,9 @@ class PrimeFactorsTest {
     void testPrimefactorof9() {
         assertEquals(Arrays.asList(3,3),primeFactor.of(9));
     }
+
+    @Test
+    void testPrimefactorof12() {
+        assertEquals(Arrays.asList(2, 2, 3),primeFactor.of(12));
+    }
 }
